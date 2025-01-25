5756c1dae458c0d7ed761b7bb2839fe29a9a6d76
Lysagxra <185969723+Lysagxra@users.noreply.github.com>
2025-01-25 15:40:06 +0100

    Update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -4,6 +4,7 @@ the main application. These utilities include functions for downloading,
 file management, URL handling, progress tracking, and more.
 
 Modules:
+    - config: Constants and settings used across the project.
     - download_utils: Functions for handling downloads.
     - file_utils: Utilities for managing file operations.
     - general_utils: Miscellaneous utility functions.
@@ -16,6 +17,7 @@ to be easily imported and used across different parts of the application.
 # helpers/__init__.py
 
 __all__ = [
+    "config",
     "download_utils",
     "file_utils",
     "general_utils",
