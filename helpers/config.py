2cafd397b802e89cb922286e200d7982660b7e67
Lysagxra <185969723+Lysagxra@users.noreply.github.com>
2025-01-25 15:40:24 +0100

    Add files via upload

---
+++ helpers/config.py
@@ -0,0 +1,10 @@
+"""
+Centralized configuration module for managing constants and settings used
+across the project. These configurations aim to improve modularity and
+readability by consolidating settings into a single location.
+"""
+
+GOFILE_API = "https://api.gofile.io"
+GOFILE_API_ACCOUNTS = f"{GOFILE_API}/accounts"
+SESSION_LOG = "session_log.txt"                 # The file used to log session
+                                                # errors.
